66f9cfe607378a67e4066bc36f4d5dd5bada0cb8
LemurdurerC <35310633+LemurdurerC@users.noreply.github.com>
2025-11-07 03:02:43 +0100

    Updated LanguageContext.tsx

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -24,6 +24,7 @@ const translations = {
     
     // Hero
     'hero.title': 'Simon & Talia',
+    'hero.subtitle': 'é',
     'hero.date': '11 juillet 2026',
     'hero.countdown.days': 'jours',
     'hero.countdown.hours': 'heures',
